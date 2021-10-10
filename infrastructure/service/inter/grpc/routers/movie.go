e96c88a3f3787a7a3516093b1262d4a132872cec
alhamsya <alhamsya@gmail.com>
2021-10-10 13:57:29 +0700

    feat(grpc): add feature grpc

--- infrastructure/service/inter/grpc/routers/movie.go
+++ infrastructure/service/inter/grpc/routers/movie.go
@@ -2,12 +2,24 @@ package grpcRouters
 
 import (
 	"context"
+	constCommon "github.com/alhamsya/boilerplate-go/domain/constants"
 
 	pb "github.com/alhamsya/boilerplate-go/protos"
 )
 
 func (grpc *GrpcServer) GetListMovie(ctx context.Context, req *pb.GetListMovieReq) (*pb.GetListMovieResp, error) {
-	panic("implement me")
+	resp, err := grpc.GrpcInteractor.GrpcInterface.DoGetListMovie(ctx, req)
+	if err != nil {
+		return &pb.GetListMovieResp{
+			Status: &pb.RPCStatus{
+				Code:    constCommon.GRPCStatusInternal,
+				Message: "",
+			},
+			Data: nil,
+		}, nil
+	}
+
+	return resp, nil
 }
 
 func (grpc *GrpcServer) GetDetailMovie(ctx context.Context, req *pb.GetDetailMovieReq) (*pb.GetDetailMovieResp, error) {
