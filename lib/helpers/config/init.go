e2827a435b9e696b85d696505946acaf07a2ae35
alhamsya.dyasta <alhamsya.dyasta@stockbit.com>
2022-03-24 00:11:56 +0700

    refactor(configure): refactor configure service

--- lib/helpers/config/init.go
+++ lib/helpers/config/init.go
@@ -14,7 +14,7 @@ import (
 //ReadConfig read config file .ini
 func (serviceConfig *ServiceConfig) ReadConfig(module string) {
 	environ := os.Getenv("ENV")
-	pathBase := fmt.Sprintf("etc/%s", "service")
+	pathBase := fmt.Sprintf("etc/%s", "configure")
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatalf("[INIT] [CONFIG] fail get working directory: %v", err)
@@ -22,13 +22,13 @@ func (serviceConfig *ServiceConfig) ReadConfig(module string) {
 
 	if environ == "" {
 		environ = "development"
-		pathBase = fmt.Sprintf("files/etc/%s", "service")
+		pathBase = fmt.Sprintf("files/etc/%s", "configure")
 	}
 
 	filePath := fmt.Sprintf("%s/%s/%s.%s.ini", dir, pathBase, module, environ)
 
 	cfg, err := ini.LoadSources(ini.LoadOptions{
-		AllowShadows: true,
+		AllowShadows:             true,
 		SpaceBeforeInlineComment: true,
 	}, filePath)
 	if err != nil {
